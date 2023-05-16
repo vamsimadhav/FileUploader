5df97dc36bd9ead16e5945fd93c2499cdb4fddac
vamsimadhav <vamsimadhav2000@gmail.com>
2023-05-16 12:36:08 +0530

    Added Google SignIn

--- app/src/main/java/com/example/fileuploader/LoginFragment.java
+++ app/src/main/java/com/example/fileuploader/LoginFragment.java
@@ -1,17 +1,51 @@
 package com.example.fileuploader;
 
+import static android.app.Activity.RESULT_OK;
+import static android.content.Context.DROPBOX_SERVICE;
+
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Scope;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.drive.Drive;
+import com.google.api.services.drive.DriveScopes;
+import com.google.android.gms.common.Scopes;
+
+import java.util.Collections;
+
 public class LoginFragment extends Fragment {
+
+    private GoogleSignInClient mGoogleSignInClient;
+    private static final String TAG = "LoginFragment";
+    private static final int RC_SIGN_IN = 9001;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -23,8 +57,65 @@ public class LoginFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Button loginButton = getActivity().findViewById(R.id.login);
 
+        mGoogleSignInClient = getGoogleSignInClient(getContext());
+
         loginButton.setOnClickListener(view1 -> {
-            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_uploadFragment);
+            signIn();
+
         });
+    }
+
+    private ActivityResultLauncher<Intent> googleSignIn = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+            handleSignInResult(task);
+        }
+    });
+
+    private GoogleSignInClient getGoogleSignInClient(Context context){
+       GoogleSignInOptions signInOptions =  new GoogleSignInOptions
+                                               .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                                               .requestEmail()
+                                               .requestScopes(new Scope(DriveScopes.DRIVE_FILE), new Scope(DriveScopes.DRIVE))
+                                               .build();
+
+        return GoogleSignIn.getClient(context,signInOptions);
+    }
+
+    private void handleSignInResult(@Nullable Task<GoogleSignInAccount> completedTask) {
+        Log.d(TAG, "handleSignInResult:" + completedTask.isSuccessful());
+
+        try {
+            // Signed in successfully, show authenticated U
+            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+            updateUI(account);
+        } catch (ApiException e) {
+            // Signed out, show unauthenticated UI.
+            Log.w(TAG, "handleSignInResult:error", e);
+            updateUI(null);
+        }
+    }
+
+    private void signIn() {
+        googleSignIn.launch(mGoogleSignInClient.getSignInIntent());
+    }
+
+    private void updateUI(@Nullable GoogleSignInAccount account) {
+        if (account != null) {
+            Navigation.findNavController(getView()).navigate(R.id.action_loginFragment_to_uploadFragment);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if the user is already signed in and all required scopes are granted
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if (account != null && GoogleSignIn.hasPermissions(account, new Scope(Scopes.DRIVE_APPFOLDER))) {
+            updateUI(account);
+        } else {
+            updateUI(null);
+        }
     }
 }
